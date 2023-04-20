6c94691f4b962d15a082e90545f1a5a99a4b86e0
madnik7 <madnik7@gmail.com>
2023-04-20 03:56:28 -0700

    code cleanup

--- GrayMint.Authorization.RoleManagement.TeamControllers/Dtos/UserRole.cs
+++ GrayMint.Authorization.RoleManagement.TeamControllers/Dtos/UserRole.cs
@@ -6,7 +6,7 @@ namespace GrayMint.Authorization.RoleManagement.TeamControllers.Dtos;
 public class UserRole : IUserRole
 {
     private readonly IUserRole _userRole;
-
+    
     public UserRole(IUserRole userRole, IUser? user)
     {
         _userRole = userRole;
@@ -14,9 +14,11 @@ public class UserRole : IUserRole
         Role = new Role(userRole.Role);
     }
 
+
     public User? User { get; }
     public string ResourceId => _userRole.ResourceId;
     public Guid UserId => _userRole.UserId;
+    public Role Role { get; } // returning an interface will cause problem for nswag client generator
     IRole IUserRole.Role => _userRole.Role;
-    public Role Role { get; }
+
 }
