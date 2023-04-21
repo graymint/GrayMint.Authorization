bff1f1979f5e843837ceb7081144912dc1cd4578
madnik7 <madnik7@gmail.com>
2023-04-21 14:57:17 -0700

    fix references

---
+++ GrayMint.Authorization/IAuthorizationImporter.cs
@@ -0,0 +1,14 @@
+﻿using GrayMint.Authorization.UserManagement.SimpleUserProviders;
+using GrayMint.Authorization.RoleManagement.SimpleRoleProviders;
+using GrayMint.Authorization.RoleManagement.RoleAuthorizations;
+using GrayMint.Authorization.RoleManagement.TeamControllers.Controllers;
+
+namespace GrayMint.Authorization;
+
+internal interface IAuthorizationImporter
+{
+    public SimpleUserProvider SimpleUserProvider { get; set; }
+    public SimpleRoleProvider SimpleRoleProvider { get; set; }
+    public RoleAuthorizationService RoleAuthorizationService { get; set; }
+    public TeamControllerBase TeamControllerBase { get; set; }
+}
