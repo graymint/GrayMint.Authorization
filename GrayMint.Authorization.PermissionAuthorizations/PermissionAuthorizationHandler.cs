64a5b80494fe3208dbffd99bd020d0c5f103bdc2
madnik7 <madnik7@gmail.com>
2023-08-19 02:54:41 -0700

    fix root access

--- GrayMint.Authorization.PermissionAuthorizations/PermissionAuthorizationHandler.cs
+++ GrayMint.Authorization.PermissionAuthorizations/PermissionAuthorizationHandler.cs
@@ -45,9 +45,12 @@ public class PermissionAuthorizationHandler : AuthorizationHandler<PermissionAut
         // get resource id 
         var resourceId = GetResourceId(context.Resource, requirement.ResourceRoute);
         var requiredClaim = PermissionAuthorization.BuildPermissionClaim(resourceId, requirement.Permission);
+        var requiredRootClaim = PermissionAuthorization.BuildPermissionClaim(AuthorizationConstants.RootResourceId, requirement.Permission);
 
         // check user has requiredClaim
-        var succeeded = context.User.Claims.Any(x => x.Type == requiredClaim.Type && x.Value == requiredClaim.Value);
+        var succeeded =
+            context.User.Claims.Any(x => x.Type == requiredClaim.Type && x.Value == requiredClaim.Value) ||
+            context.User.Claims.Any(x => x.Type == requiredRootClaim.Type && x.Value == requiredRootClaim.Value);
 
         // result
         if (succeeded)
