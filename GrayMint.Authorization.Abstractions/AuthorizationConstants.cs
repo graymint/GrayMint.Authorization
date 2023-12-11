0d7b2af15cd6d83815bc2a795309c78ecadcfdc2
madnik7 <madnik7@gmail.com>
2023-12-10 21:49:45 -0800

    add SystemUserId to AuthorizationConstants

--- GrayMint.Authorization.Abstractions/AuthorizationConstants.cs
+++ GrayMint.Authorization.Abstractions/AuthorizationConstants.cs
@@ -5,5 +5,6 @@ public static class AuthorizationConstants
     public const string RootResourceId = "*";
     public const string AnyAuthCode = "*";
     public const string DatabaseSchemePrefix = "gm";
+    public const string SystemUserId = "system";
     public static TimeSpan CacheTimeout { get; } = TimeSpan.FromMinutes(60);
 }
