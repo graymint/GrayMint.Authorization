b2a068835f2960347001dc87d6bbd1fb2dc11d3e
madnik7 <madnik7@gmail.com>
2023-12-11 01:48:53 -0800

    update program to use common app api

--- Tests/GrayMint.Authorization.Test.WebApiSample/Program.cs
+++ Tests/GrayMint.Authorization.Test.WebApiSample/Program.cs
@@ -1,16 +1,9 @@
-using GrayMint.Authorization.Authentications;
-using GrayMint.Authorization.Authentications.Controllers;
 using GrayMint.Authorization.RoleManagement.ResourceProviders;
-using GrayMint.Authorization.RoleManagement.RoleAuthorizations;
-using GrayMint.Authorization.RoleManagement.RoleProviders;
 using GrayMint.Authorization.RoleManagement.RoleProviders.Dtos;
-using GrayMint.Authorization.RoleManagement.TeamControllers;
 using GrayMint.Authorization.Test.WebApiSample.Persistence;
 using GrayMint.Authorization.Test.WebApiSample.Security;
-using GrayMint.Authorization.UserManagement.UserProviders;
 using GrayMint.Common.AspNetCore;
 using GrayMint.Common.Swagger;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 
 namespace GrayMint.Authorization.Test.WebApiSample;
@@ -23,7 +16,6 @@ public class Program
         var services = builder.Services;
 
         // options
-        var authConfiguration = builder.Configuration.GetSection("Auth");
         var appOptions = builder.Configuration.GetSection("App").Get<AppOptions>() ?? throw new Exception("Could not load AppOptions.");
         services.Configure<AppOptions>(builder.Configuration.GetSection("App"));
 
@@ -33,34 +25,9 @@ public class Program
             .AddGrayMintSwagger("Test", true);
 
         // authentication & its controller
-        services
-            .AddGrayMintAuthenticationController()
-            .AddAuthentication()
-            .AddGrayMintAuthentication(authConfiguration.Get<GrayMintAuthenticationOptions>()!, builder.Environment.IsProduction());
-
-        // authorization & its controller
-        services
-            .AddGrayMintRoleAuthorization()
-            .AddAuthorization(options =>
-            {
-                // create default policy
-                var policyBuilder = new AuthorizationPolicyBuilder();
-                policyBuilder.RequireAuthenticatedUser();
-                policyBuilder.AddAuthenticationSchemes(GrayMintAuthenticationDefaults.AuthenticationScheme);
-
-                var defaultPolicy = policyBuilder.Build();
-                options.AddPolicy("DefaultPolicy", defaultPolicy);
-                options.DefaultPolicy = defaultPolicy;
-            });
-
-        // users
-        services
-            .AddGrayMintUserProvider(authConfiguration.Get<UserProviderOptions>(), options => options.UseSqlServer(builder.Configuration.GetConnectionString("AppDatabase")));
-
-        // roles & its controller
-        services
-            .AddGrayMintTeamController(builder.Configuration.GetSection("TeamController").Get<TeamControllerOptions>())
-            .AddGrayMintRoleProvider(new RoleProviderOptions { Roles = GmRole.GetAll(typeof(Roles)) }, options => options.UseSqlServer(builder.Configuration.GetConnectionString("AppDatabase")));
+        builder.AddGrayMintCommonAuthorizationForApp(
+            GmRole.GetAll(typeof(Roles)), 
+            options => options.UseSqlServer(builder.Configuration.GetConnectionString("AppDatabase")));
 
         // nested resource controller. MUST be after role provider
         if (appOptions.UseResourceProvider)
@@ -74,9 +41,8 @@ public class Program
         webApp.UseGrayMintCommonServices(new UseServicesOptions());
         webApp.UseGrayMintSwagger(true);
         webApp.UseStaticFiles(new StaticFileOptions());
+        await webApp.UseGrayMinCommonAuthorizationForApp();
         await webApp.Services.UseGrayMintDatabaseCommand<WebApiSampleDbContext>(args);
-        await webApp.Services.UseGrayMintUserProvider();
-        await webApp.Services.UseGrayMintRoleProvider();
         if (appOptions.UseResourceProvider)
             await webApp.Services.UseGrayMintResourceProvider();
 
