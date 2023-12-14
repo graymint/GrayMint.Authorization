5640de25b53d0b0ad3f39eab5fd90f7bac223bfb
madnik7 <madnik7@gmail.com>
2023-12-13 22:23:33 -0800

    add tests for microservice

--- Tests/GrayMint.Authorization.Test.MicroServiceTest/Tests/ItemAccessTest.cs
+++ Tests/GrayMint.Authorization.Test.MicroServiceTest/Tests/ItemAccessTest.cs
@@ -1,4 +1,6 @@
 using GrayMint.Authorization.Test.MicroServiceTest.Helper;
+using GrayMint.Common.Utils;
+using System.Net;
 
 namespace GrayMint.Authorization.Test.MicroServiceTest.Tests;
 
@@ -11,6 +13,34 @@ public class ItemAccessTest
     {
         using var testInit = await TestInit.Create();
         await testInit.AppsClient.CreateAppAsync();
+    }
+
+    [TestMethod]
+    public async Task Create_an_item_by_App_writer()
+    {
+        using var testInit = await TestInit.Create();
+        var apiKey = await testInit.AuthorizationClient.ResetUserApiKeyAsync(testInit.AppId.ToString());
+        testInit.SetApiKey(apiKey);
+        var item = await testInit.ItemsClient.CreateAsync(testInit.AppId);
+        await testInit.ItemsClient.GetAsync(testInit.AppId, item.ItemId);
+    }
+
+    [TestMethod]
+    public async Task Fail_create_an_item_by_wrong_App_writer()
+    {
+        using var testInit = await TestInit.Create();
+        using var testInit2 = await TestInit.Create();
+
+        var apiKey1 = await testInit.AuthorizationClient.ResetUserApiKeyAsync(testInit.AppId.ToString());
+        var apiKey2 = await testInit.AuthorizationClient.ResetUserApiKeyAsync(testInit2.AppId.ToString());
+        testInit.SetApiKey(apiKey1);
+        var item = await testInit.ItemsClient.CreateAsync(testInit.AppId);
+        await testInit.ItemsClient.GetAsync(testInit.AppId, item.ItemId);
+
+        // try with an app write belong to other app
+        testInit.SetApiKey(apiKey2);
+        await TestUtil.AssertApiException(HttpStatusCode.Forbidden, testInit.ItemsClient.GetAsync(testInit.AppId, item.ItemId));
+
     }
 }
 
