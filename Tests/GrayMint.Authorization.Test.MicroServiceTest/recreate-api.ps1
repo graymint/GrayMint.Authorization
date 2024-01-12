7b0a3b890c4c9cd453bad07b4fcd936690d6f80a
madnik7 <madnik7@gmail.com>
2024-01-12 00:59:18 -0800

    update test api

--- Tests/GrayMint.Authorization.Test.MicroServiceTest/recreate-api.ps1
+++ Tests/GrayMint.Authorization.Test.MicroServiceTest/recreate-api.ps1
@@ -10,11 +10,3 @@ $nswagFile = "$curDir/Api/Api.nswag";
 $nswagExe = "${Env:ProgramFiles(x86)}\Rico Suter\NSwagStudio\Net80\dotnet-nswag.exe";
 $variables="/variables:namespace=$namespace,apiFile=Api.cs,projectFile=$projectFile";
 & "$nswagExe" run $nswagFile $variables;
-
-# todo: remove after nswag get fixed
-# fix beta generated code
-# load the api.cs file 
-$filePath = "$curDir/Api/Api.cs";
-$fileContent = Get-Content -Path $filePath -Raw;
-$fileContent = $fileContent -replace '"email:{email}"', '$"email:{email}"';
-$fileContent | Set-Content -Path $filePath -Force;
